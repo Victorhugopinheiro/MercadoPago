e8a04f34436de070890eeac62fa2721024bff46a
Victorhugopinheiro <azullvictorhugo10@gmail.com>
2025-04-13 21:40:08 -0300

    checkout

---
+++ app/hooks/useMercadoPago.ts
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { initMercadoPago } from "@mercadopago/sdk-react";
+import { useRouter } from "next/navigation";
+
+const useMercadoPago = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    initMercadoPago(process.env.NEXT_PUBLIC_MERCADO_PAGO_PUBLIC_KEY!);
+  }, []);
+
+  async function createMercadoPagoCheckout(checkoutData: any) {
+    try {
+      console.log(process.env.NEXT_PUBLIC_MERCADO_PAGO_PUBLIC_KEY)
+      const response = await fetch("/api/mercado-pago/create-checkout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkoutData),
+      });
+
+      const data = await response.json();
+
+      router.push(data.initPoint);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return { createMercadoPagoCheckout };
+};
+
+export default useMercadoPago;
